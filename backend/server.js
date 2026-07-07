4eb66fe737a4c7e2099d28addf09155f215c4f89
Sirinya35 <68319010035@loeitech.ac.th>
2026-07-07 12:17:54 +0700

    แก้ server.js ใหม่

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+server.on("error", (err) => {
+  console.log("SERVER ERROR:", err);
 });
